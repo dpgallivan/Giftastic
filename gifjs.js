c7ae86b59dc41e96d3e445a3f1465c16b8f19f93
Tyler Reitz <tdreitz@gmail.com>
2017-06-10 01:40:37 -0700

    feedback
    Come to office hours. There's a few things we can clear up that will get you past any obstacles you're encountering.

--- gifjs.js
+++ gifjs.js
@@ -4,8 +4,8 @@ var topics = ["Batman", "Superman", "Wonderwoman", "Aquaman", "Cyborg", "Green L
 		console.log(topics);
 	// My clicks are not working at all...and my console log isnt logging anything either
 	$("button").on("click", function() {
-		var hero = $("this").data("search");
-		console.log(x);
+		var hero = $("this").data("search");  // `this` should not be a string
+		console.log(x); // not seeing where you declared `x` in your code above
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +hero+ "&api_key=dc6zaTOxFJmzC&limit=10";
 		console.log(queryURL)
 
@@ -28,22 +28,23 @@ var topics = ["Batman", "Superman", "Wonderwoman", "Aquaman", "Cyborg", "Green L
 				heroDiv.append(heroImg)
 				heroDiv.prepend("#heroes-view");
 			}
-
+		// Your code above to this point looks good
 		$(".anotherImg").on("click", function() {
-		// Im still confused on what 'data' does 
+		// Im still confused on what 'data' does
+		// `data` is just another html attribute, but with special properties that allow you to store data on the DOM element itself. This is a bit advanced tho, so just thing of it as another html attribute for now. 
 			var state = $(this).attr("data-state");
 			if (state === 'still') {
 				// Im not sure if this is right; got from StackOverflow
-				$(this).attr('src',$(this).data('animate'));
+				$(this).attr('src', response.giphy_url); // in your `response` object from giphy, there img urls that you would want to place here. Not `$(this).data('animate')`
 				$(this).attr('data-state', 'animate');
 			} else {
-				$(this).attr('src',$(this).data('still'));
+				$(this).attr('src',$(this).data('still')); // same goes fore here
 				$(this).attr('data-state', 'still');
 			}
 		});
 	});
 
-	var animals = [''];
+	var animals = []; // no contents on initializaiton
 
     $('#addHero').on('click', function(){
     	var heroButton = $("#hero-input").val();
@@ -51,6 +52,7 @@ var topics = ["Batman", "Superman", "Wonderwoman", "Aquaman", "Cyborg", "Green L
         $("#heroesbuttons").append(newButton);
         	console.log("hello?");
 // are we suppose to put this again???
+// No, this next ajax call is unecessary. Come see me about DOM events vs. regular program flow
         queryURL = "https://api.giphy.com/v1/gifs/search?q=" + heroButton + "&api_key=dc6zaTOxFJmzC&limit=10";
         	console.log(animalButton);
 
